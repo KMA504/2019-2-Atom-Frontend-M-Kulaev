f92c39e85ec795ce4fbcaa7c80b53a389ee0b767
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-09 21:13:55 +0000

    Deploy KMA504/2019-2-Atom-Frontend-M-Kulaev to github.com/KMA504/2019-2-Atom-Frontend-M-Kulaev.git:gh-pages

---
+++ precache-manifest.7e251365afba59ae44761e148de1c7e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99c0eae59f9817b8ca5442a0281a2e67",
+    "url": "/2019-2-Atom-Frontend-M-Kulaev/index.html"
+  },
+  {
+    "revision": "b0fa68983488acffb7cc",
+    "url": "/2019-2-Atom-Frontend-M-Kulaev/static/css/main.b4eae362.chunk.css"
+  },
+  {
+    "revision": "5a7cc06811c2f8722dd0",
+    "url": "/2019-2-Atom-Frontend-M-Kulaev/static/js/2.d2d31149.chunk.js"
+  },
+  {
+    "revision": "b0fa68983488acffb7cc",
+    "url": "/2019-2-Atom-Frontend-M-Kulaev/static/js/main.da4f71cc.chunk.js"
+  },
+  {
+    "revision": "7487dab26dfbb1f3d19e",
+    "url": "/2019-2-Atom-Frontend-M-Kulaev/static/js/runtime-main.70170fe6.js"
+  },
+  {
+    "revision": "a55f1c4358068374afaef45a15ab0eb8",
+    "url": "/2019-2-Atom-Frontend-M-Kulaev/static/media/logo.a55f1c43.svg"
+  }
+]);
